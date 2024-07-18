8834845dd787950a3407401356873d2cbc946938
Tuditi <daviddetroch@pm.me>
2024-07-18 18:34:30 +0200

    wip: part two (memory allocation is failing)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,5 +1,6 @@
 advent_of_code::solution!(5);
 
+#[derive(Debug)]
 struct MapLine {
     destination: i64,
     source: i64,
@@ -51,7 +52,10 @@ fn parse_input(input: &str) -> (Vec<i64>, Vec<Vec<MapLine>>) {
 }
 
 fn go_through_maps(maps: &Vec<Vec<MapLine>>, mut input: i64) -> u32 {
+    println!("NEW INPUT {input}");
+    // println!("");
     maps.iter().for_each(|current_map| {
+        // println!("Input: {input}, Current Map: {:?}", current_map);
         input = transform(current_map, &input);
     });
     input as u32
@@ -65,22 +69,50 @@ fn transform(map: &Vec<MapLine>, input: &i64) -> i64 {
             destination,
         } = line;
         let diff = *input - *source as i64;
-        if diff > 0 && diff < *range as i64 {
+        if diff >= 0 && diff < *range as i64 {
             return (*destination as i64) + diff;
         }
     }
     *input
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (seeds, maps) = parse_input(input);
+fn calculate_seeds_from_ranges(ranges: Vec<i64>) -> Vec<i64> {
+    let length = ranges.len();
+    let total_size = ranges.iter().step_by(2).map(|&range| range as usize).sum();
+    let mut seeds = Vec::with_capacity(total_size);
+    println!("start {length}");
+    for i in 0..length / 2 {
+        println!("{i}");
+        let start_seed = ranges[2 * i];
+        println!("a");
+
+        let range = ranges[2 * i + 1];
+        // let next_seeds = Vec::with_capacity(range);
+        println!("b");
+        seeds.extend(start_seed..start_seed + range);
+        println!("c");
+    }
+    seeds
+}
+
+fn calculate_closest_location(seeds: Vec<i64>, maps: Vec<Vec<MapLine>>) -> Option<u32> {
     let locations = seeds.iter().map(|seed| go_through_maps(&maps, *seed));
     let closest_location = locations.min();
     closest_location
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    let (seeds, maps) = parse_input(input);
+    calculate_closest_location(seeds, maps)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    println!("Parse input");
+    let (seed_ranges, maps) = parse_input(input);
+    println!("Seed_ranges");
+    let seeds = calculate_seeds_from_ranges(seed_ranges);
+    println!("Seeds: {:?}", seeds);
+    calculate_closest_location(seeds, maps)
 }
 
 #[cfg(test)]
@@ -96,7 +128,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
 
