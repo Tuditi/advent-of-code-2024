36cbed1d5d92780ce2648611418e34694f6e817f
Tuditi <daviddetroch@pm.me>
2024-08-05 17:49:09 +0200

    chore: cleanup

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,81 +1,77 @@
 use num::integer::lcm;
+use regex::Regex;
 use std::collections::HashMap;
+use std::iter::Cycle;
+use std::str::Chars;
 advent_of_code::solution!(8);
 
-type Direction<'a> = Vec<&'a str>;
+type Direction<'a> = (&'a str, &'a str);
 
-fn parse_input<'a>(
-    input: &'a str,
-) -> (
-    impl Iterator<Item = char> + std::clone::Clone + 'a,
-    HashMap<&str, Direction<'a>>,
-) {
+fn parse_input<'a>(input: &'a str) -> (Cycle<Chars>, HashMap<&str, Direction<'a>>) {
     let (first_line, other_lines) = input.split_once('\n').unwrap();
     let desert_map = create_desert_map(other_lines);
-    return (first_line.chars(), desert_map);
+    return (first_line.chars().cycle(), desert_map);
 }
 
 fn create_desert_map(input: &str) -> HashMap<&str, Direction> {
+    let re = Regex::new(r"^([0-9A-Z]{3}) = \(([0-9A-Z]{3}), ([0-9A-Z]{3})\)$").unwrap();
     let mut desert_map: HashMap<&str, Direction> = HashMap::new();
-    input.lines().filter(|l| !l.is_empty()).for_each(|l| {
-        let (current_pos, next_pos) = l.split_once('=').unwrap();
-        let directions = next_pos.split_once(',').unwrap();
-        let directions = [directions.0, directions.1]
-            .iter_mut()
-            .map(|direction| direction.trim_matches(|c: char| !c.is_alphanumeric()))
-            .collect();
-        desert_map.insert(current_pos.trim(), directions);
+
+    let lines = input.lines().skip(1);
+    lines.for_each(|l| {
+        let captures = re.captures(l).unwrap();
+        let node = captures.get(1).unwrap().as_str();
+        let left = captures.get(2).unwrap().as_str();
+        let right = captures.get(3).unwrap().as_str();
+        desert_map.insert(node, (left, right));
     });
     desert_map
 }
 
-fn get_next_pos<'a>(input: char, directions: &Direction<'a>) -> &'a str {
+fn get_next_pos<'a>(input: char, left: &'a str, right: &'a str) -> &'a str {
     match input {
-        'L' => directions[0],
-        'R' => directions[1],
+        'L' => left,
+        'R' => right,
         _ => panic!("Invalid instruction"),
     }
 }
 
 fn go_through_desert(
     start_position: &str,
-    instructions: impl Iterator<Item = char> + std::clone::Clone,
+    instructions: &mut Cycle<Chars>,
     map: HashMap<&str, Direction>,
 ) -> Option<u32> {
     let mut count = 0;
     let mut current_pos = start_position;
-    for c in instructions.cycle() {
+    loop {
+        let c = instructions.next().unwrap();
         count += 1;
-        let directions = map.get(current_pos).unwrap();
-        current_pos = get_next_pos(c, directions);
+        let (left, right) = map.get(current_pos).unwrap();
+        current_pos = get_next_pos(c, left, right);
         if current_pos == "ZZZ" {
             return Some(count);
         }
     }
-    unreachable!()
 }
 
-fn ghost_through_desert(
-    instructions: impl Iterator<Item = char> + std::clone::Clone,
-    map: HashMap<&str, Direction>,
-) -> Option<u64> {
+fn ghost_through_desert(instructions: Cycle<Chars>, map: HashMap<&str, Direction>) -> Option<u64> {
     let mut current_positions: Vec<&str> = map
         .keys()
         .filter(|pos| (pos).ends_with('A'))
-        .cloned()
+        .copied()
         .collect();
     let mut count = 0;
     let mut found_pos: Vec<&str> = vec![];
     let mut iteration_count: Vec<u64> = vec![];
-    for c in instructions.cycle() {
+    for c in instructions {
         if current_positions.is_empty() {
             break;
         }
         count += 1;
 
         current_positions.iter_mut().for_each(|ghost_pos| {
-            let directions = map.get(ghost_pos).unwrap();
-            *ghost_pos = get_next_pos(c, directions);
+            let (left, right) = map.get(ghost_pos).unwrap();
+            *ghost_pos = get_next_pos(c, left, right);
 
             if ghost_pos.ends_with('Z') {
                 iteration_count.push(count);
@@ -100,8 +96,8 @@ fn remove_found_pos(found_pos: &mut Vec<&str>, current_positions: &mut Vec<&str>
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (instructions, map) = parse_input(input);
-    go_through_desert("AAA", instructions, map)
+    let (mut instructions, map) = parse_input(input);
+    go_through_desert("AAA", &mut instructions, map)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
