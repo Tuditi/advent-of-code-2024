d09bd1a2386c2babd9dbcc389d061c3e02508637
David De Troch <tuditi@Davids-MacBook-Pro.local>
2025-01-10 14:38:41 +0100

    feat: add Direction to map

--- src/utils/map.rs
+++ src/utils/map.rs
@@ -1,7 +1,19 @@
-#[derive(Copy, Clone, Debug, PartialEq)]
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Position {
     pub x: isize,
     pub y: isize,
+}
+
+impl Position {
+    pub fn new(x: usize, y: usize) -> Self {
+        Position {
+            x: x as isize,
+            y: y as isize,
+        }
+    }
 }
 
 pub struct Map {}
@@ -39,6 +51,52 @@ impl Navigate for Position {
         Self {
             x: self.x - 1,
             y: self.y,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, EnumIter)]
+pub enum Direction {
+    Down,
+    Right,
+    Left,
+    Up,
+}
+
+impl From<Direction> for usize {
+    fn from(direction: Direction) -> Self {
+        match direction {
+            Direction::Up => 0,
+            Direction::Down => 1,
+            Direction::Left => 2,
+            Direction::Right => 3,
+        }
+    }
+}
+
+struct DirectionCollection(Vec<Direction>);
+impl FromIterator<Direction> for DirectionCollection {
+    fn from_iter<T: IntoIterator<Item = Direction>>(iter: T) -> Self {
+        DirectionCollection(iter.into_iter().collect())
+    }
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Self {
+        match self {
+            &Direction::Down => Direction::Up,
+            &Direction::Up => Direction::Down,
+            &Direction::Left => Direction::Right,
+            &Direction::Right => Direction::Left,
+        }
+    }
+
+    pub fn get_next_pos(&self, cur_pos: &Position) -> Position {
+        match &self {
+            Direction::Up => cur_pos.up(),
+            Direction::Down => cur_pos.down(),
+            Direction::Left => cur_pos.left(),
+            Direction::Right => cur_pos.right(),
         }
     }
 }
